2c51202b6f5d8f5379d1e4b7f7bdf9486bddfff7
ParthaManna <manna79partha@gmail.com>
2021-12-30 15:29:32 +0530

    models.py updated

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
+
 
 # Create your models here.
+class Author(models.Model):
+    first_name=models.CharField(max_length=30)
+    last_name=models.CharField(max_length=30)
+    email_address=models.EmailField()
+    def __str__(self): 
+        return f"{self.first_name} {self.last_name}"
+
+class Tag(models.Model):
+    caption=models.CharField(max_length=25)
+    def __str__(self): 
+        return f"{self.caption}"
+
+class Post(models.Model):
+    title=models.CharField(max_length=40)
+    excerpt=models.CharField(max_length=50)
+    image_name=models.CharField(max_length=20)
+    date=models.DateField(auto_now=True)
+    slug=models.SlugField(unique=True)
+    content=models.TextField(max_length=2000,validators=[MinLengthValidator(15)])
+    author=models.ForeignKey(Author,on_delete=models.SET_NULL,null=True)
+    tags=models.ManyToManyField(Tag)
+
--- blogapp/urls.py
+++ blogapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.starting_page,name="starting-page"),
+    path(' ', views.starting_page,name="starting-page"),
     path('post/',views.post,name="post-page"),
     path('post/<slug:slug>',views.post_details,name="post-details")
 ]
